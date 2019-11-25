51542fef55384fc93f94de4da2c769a39c968645
ChangGengWu <a8040333@gmail.com>
2019-11-25 22:29:48 +0800

    name to chinese only

--- R/dbtest.R
+++ R/dbtest.R
@@ -1,18 +1,18 @@
 #install.packages("DBI")
 #install.packages("odbc")
-install.packages("RMySQL")
 library("RMySQL");
+library("visNetwork");
 #連結資料庫
 con = dbConnect(MySQL(), user = 'root', password = '12345678', dbname = 'test1',
                             host = 'localhost')
 #轉碼(讓中文顯示)
 dbSendQuery(con,"SET NAMES big5")
 #query
-result = dbSendQuery(con, "select * from test WHERE id=4")
+result = dbSendQuery(con, "SELECT * FROM site WHERE id BETWEEN 'S0002' AND 'S0080'")
 #執行query
 data.frame = fetch(result)
 x <- data.frame
-x$string
-nodes <- data.frame(id = c(x$id),label = c(x$string))
-visNetwork(nodes,idth = "100%",height = "500px")%>%
+
+nodes <- data.frame(id = c(x$id),label = c(x$name), shape='circle')
+visNetwork(nodes,width = "100%",height = "500px")%>%
   visEdges(smooth = FALSE)
